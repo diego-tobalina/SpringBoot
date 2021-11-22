d9042306dffdea9cae5f7e6cc6a223a54a1684d0
Diego Tobalina <diegotobalina97@gmail.com>
2021-11-22 15:56:14 +0100

    Better logs

---
+++ src/main/java/com/xbidi/spring/log/RequestIdFilter.java
@@ -0,0 +1,32 @@
+package com.xbidi.spring.log;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.UUID;
+
+/** @author diegotobalina created on 24/06/2020 */
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class RequestIdFilter extends OncePerRequestFilter {
+
+  @Value("${request.id.header}")
+  public String requestIdHeader;
+
+  @Override
+  protected void doFilterInternal(
+      @NotNull HttpServletRequest req, HttpServletResponse res, FilterChain chain)
+      throws ServletException, IOException {
+    res.setHeader(requestIdHeader, UUID.randomUUID().toString());
+    chain.doFilter(req, res);
+  }
+}
